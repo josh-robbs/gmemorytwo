204360eefae22ff7f0c43381c41f7e5f705e7aa0
Josh Robbs <joshrobbs@Zuul.local>
2018-07-31 14:49:55 -0600

    migrated and seeded database

---
+++ migrations/20180731141227_coffee.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('coffee', coffee => {
+    coffee.increments()
+    coffee.string('name')
+    coffee.string('roaster')
+    coffee.integer('aroma')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('coffee')
+};
